1ce607436cd86c6a4e0e8fb18961447c1c58d026
Priyankateke <priiyankateke1705@gmail.com>
2020-05-04 09:34:03 -0700

    ADDED: Code & TestCase For Equality Check On 0 Feet

--- src/test/java/quantitymeasurement/TestQuantityMeasurement.java
+++ src/test/java/quantitymeasurement/TestQuantityMeasurement.java
@@ -1,5 +1,22 @@
 package quantitymeasurement;
 
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 public class TestQuantityMeasurement {
+    static FeetMeasurement feetMeasurement;
+
+    @BeforeClass
+    public static void beforeClass() {
+        feetMeasurement = new FeetMeasurement();
+    }
+
+    @Test
+    public void givenZeroFeetAndZeroFeetValue_WhenEqual_ShouldReturnTrue() {
+        double value1 = feetMeasurement.getFeet(0.0);
+        double value2 = feetMeasurement.getFeet(0.0);
+        Assert.assertEquals(value1, value2, 0.0);
+    }
 
 }
