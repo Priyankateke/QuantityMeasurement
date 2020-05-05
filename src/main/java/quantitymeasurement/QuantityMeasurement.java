26a85419207900cc217f36c2afacb926ca1bd72b
Priyankateke <priiyankateke1705@gmail.com>
2020-05-04 23:54:47 -0700

    ADDED: Test Case to Check Null Inch

---
+++ src/main/java/quantitymeasurement/QuantityMeasurement.java
@@ -0,0 +1,29 @@
+package quantitymeasurement;
+
+public class QuantityMeasurement {
+    private double feet;
+    private double inch;
+
+    public double getFeet(Double inch) {
+        return inch/12;
+    }
+
+    public double getInch(Double feet) {
+        return feet*12;
+    }
+
+    /**
+     * Object Equal Method
+     * @param o
+     * @return
+     */
+    @Override
+    public boolean equals(Object o) {
+        if(o == null) return false;
+        if(this == o) return true;
+        if (this.getClass() == o.getClass()) return true;
+        QuantityMeasurement that = (QuantityMeasurement) o;
+        return Double.compare(that.feet, feet) == 0 &&
+                Double.compare(that.inch, inch) == 0;
+    }
+}
--- src/test/java/quantitymeasurement/TestQuantityMeasurement.java
+++ src/test/java/quantitymeasurement/TestQuantityMeasurement.java
@@ -5,11 +5,11 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class TestQuantityMeasurement {
-    static FeetMeasurement feetMeasurement;
+    static QuantityMeasurement quantityMeasurement;
 
     @BeforeClass
     public static void beforeClass() {
-        feetMeasurement = new FeetMeasurement();
+        quantityMeasurement = new QuantityMeasurement();
     }
 
     /**
@@ -17,8 +17,8 @@ public class TestQuantityMeasurement {
      */
     @Test
     public void givenZeroFeetAndZeroFeetValue_WhenEqual_ShouldReturnTrue() {
-        double value1 = feetMeasurement.getFeet(0.0);
-        double value2 = feetMeasurement.getFeet(0.0);
+        double value1 = quantityMeasurement.getFeet(0.0);
+        double value2 = quantityMeasurement.getFeet(0.0);
         Assert.assertEquals(value1, value2, 0.0);
     }
 
@@ -27,16 +27,21 @@ public class TestQuantityMeasurement {
      */
     @Test
     public void givenNullFeetValue_ShouldReturnFalse() {
-        Assert.assertFalse(feetMeasurement.equals(null));
+        Assert.assertFalse(quantityMeasurement.equals(null));
     }
 
     /**
      * Reference Check
      */
     @Test
-    public void givenReferenceObject_WhenEqual_ShouldReturnTrue() {
-        FeetMeasurement feetMeasurement2 = new FeetMeasurement();
-        Assert.assertEquals(feetMeasurement, feetMeasurement2);
+    public void givenReferenceObjectOfFeet_WhenEqual_ShouldReturnTrue() {
+        QuantityMeasurement quantityMeasurement2 = new QuantityMeasurement();
+        Assert.assertEquals(quantityMeasurement, quantityMeasurement2);
+    }
+
+    @Test
+    public void givenSameClassObjectOfFeet_WhenEqual_ShouldReturnTrue() {
+        Assert.assertTrue(quantityMeasurement.equals(quantityMeasurement));
     }
 
     /**
@@ -44,8 +49,8 @@ public class TestQuantityMeasurement {
      */
     @Test
     public void givenTwoDifferentFeetValues_WhenEqual_ShouldReturnFalse() {
-        double value1 = feetMeasurement.getFeet(0.0);
-        double value2 = feetMeasurement.getFeet(1.0);
+        double value1 = quantityMeasurement.getFeet(0.0);
+        double value2 = quantityMeasurement.getFeet(1.0);
         Assert.assertNotEquals(value1, value2, 0.0);
     }
 
@@ -54,8 +59,26 @@ public class TestQuantityMeasurement {
      */
     @Test
     public void givenTwoEqualFeetValues_WhenEqual_ShouldReturnTrue() {
-        double value1 = feetMeasurement.getFeet(1.0);
-        double value2 = feetMeasurement.getFeet(1.0);
+        double value1 = quantityMeasurement.getFeet(1.0);
+        double value2 = quantityMeasurement.getFeet(1.0);
         Assert.assertEquals(value1, value2, 0.0);
+    }
+
+    /**
+     * Given 0 Inch and 0 Inch Should Return equal
+     */
+    @Test
+    public void givenZeroInchAndZeroInchValue_WhenEqual_ShouldReturnTrue() {
+        double value1 = quantityMeasurement.getInch(0.0);
+        double value2 = quantityMeasurement.getInch(0.0);
+        Assert.assertEquals(value1, value2, 0.0);
+    }
+
+    /**
+     * Null check For Inch
+     */
+    @Test
+    public void givenNullInchValue_ShouldReturnFalse() {
+        Assert.assertFalse(quantityMeasurement.equals(null));
     }
 }
